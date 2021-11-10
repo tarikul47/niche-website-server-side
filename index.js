0ad07a1a3f15cfa3c6bf1c2d80c39dababfd52a3
Tarikul Islam <tarikul47@gmail.com>
2021-11-10 21:39:43 +0600

    Server setup

---
+++ index.js
@@ -0,0 +1,26 @@
+
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.port || 5000;
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res)=> {
+    res.send('Running Shop Grid Root API on Browser');
+});
+
+app.listen(port, () => {
+    console.log('Running Shop Grid Server in console', port);
+});
+
+
+

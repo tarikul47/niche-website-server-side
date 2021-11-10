7b95ba345bc0d464e0d36cdd93f971e7cdd15174
Tarikul Islam <tarikul47@gmail.com>
2021-11-10 22:08:08 +0600

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
-const { MongoClient } = require('mongodb');
-const ObjectId = require('mongodb').ObjectId;
+//const { MongoClient } = require('mongodb');
+//const ObjectId = require('mongodb').ObjectId;
 
 const cors = require('cors');
 require('dotenv').config();
